4765541c75e62258fbb7190dde9e3292b9575366
nkpiksalia <nkpiksalia@gmail.com>
2026-07-26 17:56:31 +0200

    Własne style bloku Grupa i Przycisk

--- functions.php
+++ functions.php
@@ -35,3 +35,25 @@ function fse_theme_setup(): void {
 	add_editor_style( 'style.css' );
 }
 add_action( 'after_setup_theme', 'fse_theme_setup' );
+
+/**
+ * Register custom styles for core blocks.
+ */
+function fse_theme_register_block_styles(): void {
+	register_block_style(
+		'core/group',
+		array(
+			'name'  => 'fse-card',
+			'label' => __( 'Karta', 'fse-theme' ),
+		)
+	);
+
+	register_block_style(
+		'core/button',
+		array(
+			'name'  => 'fse-arrow',
+			'label' => __( 'Ze strzałką', 'fse-theme' ),
+		)
+	);
+}
+add_action( 'init', 'fse_theme_register_block_styles' );
